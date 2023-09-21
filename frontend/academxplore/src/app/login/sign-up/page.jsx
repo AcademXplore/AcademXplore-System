9a37f3678ad1883ac5f1a6e06cbc3c3bab43d386
DaniRiber <daniribeirolc@hotmail.com>
2023-09-20 23:32:41 -0300

    Criação do piloto da sign-up

--- frontend/academxplore/src/app/login/sign-up/page.jsx
+++ frontend/academxplore/src/app/login/sign-up/page.jsx
@@ -1,16 +1,26 @@
+import ButtonSubmit from '@/components/ButtonSubmit/ButtonSubmit';
 import InputLogin from '../../../components/InputLogin/InputLogin'
 import "../login.css"
 
 export default function SignUp() {
   return (
-    <main className="d-flex w-100 vh-100 background-primary-color">
-      {/* <ButtonSelectionSignInOrSignUp/> */}
-      <form action="">
-        <InputLogin iconBootstrap="bi bi-person" placeholder="Nome" type="password"/>
-        <InputLogin iconBootstrap="bi bi-person-vcard" placeholder="CPF" type="text"/>
-        <InputLogin iconBootstrap="bi bi-envelope" placeholder="E-mail" type="email"/>
-        {/* <ButtonSubmitForm/> */}
-      </form>
+    <main className="d-flex w-100 h-100">
+      <div>
+        {/* <ButtonSelectionSignInOrSignUp/> */}
+      </div>
+      <div className="w-100 h-100 justify-content-center align-items-center d-flex align-items-end flex-column">  
+        <form className="w-100 h-50 justify-content-center align-items-center d-flex flex-column" action="">
+          <InputLogin iconBootstrap="bi bi-person" placeholder="Nome" type="text"/>
+          <InputLogin iconBootstrap="bi bi-person-vcard" placeholder="CPF" type="text"/>
+          <InputLogin iconBootstrap="bi bi-envelope" placeholder="E-mail" type="email"/>
+          <InputLogin iconBootstrap="bi bi-mortarboard" placeholder="Instituição" type="text"/>
+          <InputLogin iconBootstrap="bi bi-person-video3" placeholder="Aluno/Professor" type="text"/>
+          <InputLogin iconBootstrap="bi bi-person-badge" placeholder="Matrícula" type="text"/>
+          <InputLogin iconBootstrap="bi bi-lock" placeholder="Senha" type="password"/>
+          <InputLogin iconBootstrap="bi bi-lock" placeholder="Confirmar Senha" type="password"/>
+          <ButtonSubmit/>
+        </form>
+      </div>
     </main>
   );
 }
