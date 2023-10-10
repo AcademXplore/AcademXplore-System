76dcbd1d5262a9dee6111d68550b3d694442a947
Daniel Camara <daniel.camara@liferay.com>
2023-10-09 21:54:25 -0300

    DATABASE - Add new Column

--- backend/academxplore/src/main/java/com/academxplore/academxplore/models/Notificacao.java
+++ backend/academxplore/src/main/java/com/academxplore/academxplore/models/Notificacao.java
@@ -28,6 +28,7 @@ public class Notificacao {
     private String descricao;
     @Column(nullable = false)
     private Date dataCriacao;
+    @Column(columnDefinition = "TINYINT(3)")
     private TipoNotificacao tipo;
     @Column(nullable = false)
     private Status status;
