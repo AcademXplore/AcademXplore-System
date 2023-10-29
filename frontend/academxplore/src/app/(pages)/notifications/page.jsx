8d706cc179a2861f804091ce3ed1c36f8cc0de75
gabrielaraujog9 <gabrielaraujogoncalves14@gmail.com>
2023-10-29 00:05:53 -0300

    retirar imagem da notificação

--- frontend/academxplore/src/app/(pages)/notifications/page.jsx
+++ frontend/academxplore/src/app/(pages)/notifications/page.jsx
@@ -59,9 +59,9 @@ export default function Notification() {
           data?.map(notification => {
             return(
               <div onClick={() => atualizarNotificacao(notification.id)} key={notification.id} id={notification.id} className="w-100 py-2 px-5 gap-3 position-relative align-items-center text-light rounded-3 d-flex" style={{cursor: "pointer",backgroundColor: notification.status == "Ativo" ? "var(--secundary-color)" : "var(--primary-color)"}}>
-                <div className="justify-content-center d-flex" style={{minHeight: "60px", minWidth: "60px", borderRadius: "100%", backgroundColor: `rgb(${gerarNumeroAleatorio()}, ${gerarNumeroAleatorio()}, ${gerarNumeroAleatorio()})`}}>
-                  <h2 className="align-self-center my-auto">FB</h2>
-                </div>
+                {/* <div className="justify-content-center d-flex" style={{minHeight: "60px", minWidth: "60px", borderRadius: "100%", backgroundColor: `rgb(${gerarNumeroAleatorio()}, ${gerarNumeroAleatorio()}, ${gerarNumeroAleatorio()})`}}>
+                  <h2 className="align-self-center my-auto">{notification.tipo != "CRIACAO" ? `${notification.projetoTitulo[0].toUpperCase()}${notification.projetoTitulo[1].toUpperCase()}` : "BV"}</h2>
+                </div> */}
                 <div className="text-wrap">
                   <span>{notification.descricao}</span>
                   <div style={{right: "20px", fontSize: "12px"}} className="position-absolute bottom-0">
