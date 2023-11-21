450da139c8a987501a1c860e34a10cb2a8afa1ec
gabrielaraujog9 <gabrielaraujogoncalves14@gmail.com>
2023-11-21 20:12:08 -0300

    tabBar configurada

--- mobile/academxplore/src/app/(app)/(tabs)/_layout.jsx
+++ mobile/academxplore/src/app/(app)/(tabs)/_layout.jsx
@@ -1,17 +1,29 @@
 import { useAuth } from '@/src/contexts/auth-context';
-import { Redirect, Stack, Tabs } from 'expo-router';
+import { Tabs } from 'expo-router';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { Button } from 'react-native';
 
 export default function AppLayout() {
   const {authState, onLogout} = useAuth()
 
   return (
-    <Tabs screenOptions={{headerStyle: {backgroundColor: "#024443"}, headerTitleStyle: {color: "#fff"}}}>
-      <Tabs.Screen name='index' options={{title: "Linha do Tempo", headerLeft: () => <Button title='sign out' onPress={onLogout}/>}}/>
-      <Tabs.Screen name='my-projects' options={{title: "Meus Projetos"}}/>
-      <Tabs.Screen name='candidacy' options={{title: "Candidaturas", tabBarItemStyle: {display: authState.user.perfil == 'ALUNO' ? 'flex' : 'none'}}}/>
-      <Tabs.Screen name='notifications' options={{title: "Notificações"}}/>
-      <Tabs.Screen name='profile' options={{title: "Perfil"}}/>
+    <Tabs screenOptions={
+        {
+          headerStyle: {backgroundColor: "#024443"}, 
+          headerTitleStyle: {color: "#fff"}, 
+          tabBarStyle: {backgroundColor: "#024443"}, 
+          tabBarActiveBackgroundColor: "#fff", 
+          tabBarActiveTintColor: "#024443",
+          tabBarInactiveTintColor: "#fff",
+          tabBarItemStyle: {marginVertical: 2, marginHorizontal: 5, borderRadius: 5}
+        }
+      }
+    >
+      <Tabs.Screen name='index' options={{title: "Linha do Tempo", headerLeft: () => <Button title='sign out' onPress={onLogout}/>, tabBarIcon: ({color, size}) => <MaterialCommunityIcons name="home-outline" size={size} color={color} />}}/>
+      <Tabs.Screen name='my-projects' options={{title: "Meus Projetos", tabBarIcon: ({color, size}) => <MaterialCommunityIcons name="google-analytics" size={size} color={color} />}}/>
+      <Tabs.Screen name='candidacy' options={{title: "Candidaturas", tabBarItemStyle: {display: authState.user.perfil == 'ALUNO' ? 'flex' : 'none'}, tabBarIcon: ({color, size}) => <MaterialCommunityIcons name="account-supervisor-outline" size={size} color={color} />}}/>
+      <Tabs.Screen name='notifications' options={{title: "Notificações", tabBarIcon: ({color, size}) => <MaterialCommunityIcons name="bell-outline" size={size} color={color} />}}/>
+      <Tabs.Screen name='profile' options={{title: "Perfil", tabBarIcon: ({color, size}) => <MaterialCommunityIcons name="account-circle-outline" size={size} color={color}/>}}/>
     </Tabs>
   );
 }
