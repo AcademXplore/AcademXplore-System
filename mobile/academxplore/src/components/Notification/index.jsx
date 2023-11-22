0db6f3c2323d69edbdf08c6431ad1af0bd9df961
DaniRiber <daniribeirolc@hotmail.com>
2023-11-21 23:27:01 -0300

    Ajustes no componente de notificações

--- mobile/academxplore/src/components/Notification/index.jsx
+++ mobile/academxplore/src/components/Notification/index.jsx
@@ -3,11 +3,11 @@ import { styles } from "./styles";
 
 export function Notification ({status, description, date, onPress}) {
   return (
-      <Pressable style={styles.container} onPress={onPress}>
+      <Pressable style={[styles.container, {backgroundColor: status=="Ativo" ? "#00B8B5" : "#006B69"} ]} onPress={onPress}>
         <View style={styles.card}>
-          <Text style={styles.description}></Text>  
+          <Text style={styles.description}> {description} </Text>  
           <View style={styles.dateContainer}>
-            <Text style={styles.date}>  </Text>
+            <Text style={styles.date}> {new Date(date).toLocaleString()} </Text>
           </View>
         </View>
       </Pressable>
--- mobile/academxplore/src/components/Notification/styles.js
+++ mobile/academxplore/src/components/Notification/styles.js
@@ -2,18 +2,30 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
     container: {
+        display: "flex",
+        flexWrap: "wrap",
       borderRadius: 12,
-      padding: 16,
-      borderWidth: 1,
+      padding: 8,
       marginVertical: 12,
       marginHorizontal: 12,
-      borderColor: "gray",
-      flexDirection: 'column',
+      flexDirection: 'row',
       alignItems: 'center',
-      backgroundColor: "#fff",
-      flexDirection: "row"
     },
-    text: {
-        
+    card: {
+        flexWrap: "wrap",
+        width: "100%",
+    },
+    dateContainer: {
+        right: 2,
+        position: 'absolute',
+        bottom: 0,
+    },
+    description: {
+        width: "100%",
+        marginBottom: 15,
+        color: "white"
+    },
+    date: {
+        color: "white"
     }
   });
